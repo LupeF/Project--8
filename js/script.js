0a0e924b5a1fa5d4203c280f80736f12729e4626
LupeF <46634202+LupeF@users.noreply.github.com>
2025-01-29 16:09:45 -0800

    add input and search functionality

--- js/script.js
+++ js/script.js
@@ -4,12 +4,13 @@ const grid = document.querySelector('.grid-container');
 const card = document.querySelector('.card');
 const overlay = document.querySelector('.overlay');
 const modal = document.querySelector('.modal');
+const input = document.getElementById('search');
 
 
 //************************************************ */
 //* functions that create the card for each employee
 //********************************************** */
-function generateCard(employee) { 
+function generateCard(employee) {                   //function that creates the card
     employee.results.forEach((employee, index) => {
         grid.innerHTML += `
         <div class="card" data-index="${index}">
@@ -25,7 +26,7 @@ function generateCard(employee) {
     })
 }
 
-const generateModal = (person) => {
+const generateModal = (person) => {              //function that creates the modal
     const birthdate = new Date(person.dob.date);
     overlay.innerHTML = `
         <div class="modal">
@@ -39,7 +40,7 @@ const generateModal = (person) => {
                     <hr />
                     <p>${person.phone}</p>
                     <p class="address"> ${person.location.street.number} ${person.location.street.name}, ${person.location.state} ${person.location.postcode}</p>
-                    <p>Birthday: ${birthdate.getMonth()}/${birthdate.getDate()}/${birthdate.getFullYear()}</p>
+                    <p>Birthday: ${birthdate.getMonth() + 1}/${birthdate.getDate()}/${birthdate.getFullYear()}</p>
                 </div>
             </div>
         </div>
@@ -62,7 +63,7 @@ function checkStatus(response) {
       return Promise.reject(new Error(response.statusText));
     }
   }
-function fetchData(url) {
+function fetchData(url) {                
     fetch(url)
     .then(checkStatus)
     .then(res => res.json())
@@ -75,7 +76,7 @@ fetchData(urlAPI);
 //************************************************ */
 //* Event listener for the modal
 //********************************************** */
-grid.addEventListener('click', (e) => {
+grid.addEventListener('click', (e) => {         //event listener for the modal
     const card = e.target.closest('.card')
     if(card){
         const index = card.getAttribute('data-index');
@@ -83,4 +84,17 @@ grid.addEventListener('click', (e) => {
         generateModal(person);
     }
 });
+
+input.addEventListener('keyup', (e) => {        //search functionality
+    const search = input.value.toLowerCase();
+    const cards = document.querySelectorAll('.card');
+    cards.forEach(card => {
+        const name = card.querySelector('.name').textContent.toLowerCase();
+        if(name.includes(search)){
+            card.style.display = 'flex';
+        } else {
+            card.style.display = 'none';
+        }
+    });
+})
 
